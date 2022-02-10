aedc174c143e5425e7f5c22a2b66d6c285525713
KR <kyranrawlinson@gmail.com>
2022-02-10 18:22:08 +0000

    Added scripts/get_weth.py

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -9,7 +9,7 @@ def get_weth():
     weth = interface.IWeth(config['networks'][network.show_active()]['weth_address'])
     tx = weth.deposit({'from': account, 'value': 0.1 * (10**18)})
     tx.wait(1)
-    print('Receieved 0.1 WETH')
+    print('Receieved 0.1 WETH ...\n')
 
 
 
